a79945c21f4e1cfb27494d01e1e4be047295eb42
Alex Muench <ammuench@gmail.com>
2020-07-21 17:58:25 -0500

    ➕ Add TS scaffolding, base packages

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export class AMMuenchTSApp {
+	public helloWorld() {
+		return '🦈   Hello World, I am a typescript app';
+	}
+}
+const myApp = new AMMuenchTSApp();
+
+console.log(myApp.helloWorld());
