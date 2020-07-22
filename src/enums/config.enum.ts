56f0a0e55e04d9c65d2183727a20557f1bec6050
Alex Muench <ammuench@gmail.com>
2020-07-21 20:32:22 -0500

    ✨ Add in config generation, value parser * Create parser to get style to SQL value * Create config generator * Create enum file for config ids

---
+++ src/enums/config.enum.ts
@@ -0,0 +1,7 @@
+export enum CONFIG_IDS {
+  CLIENT = "#csssql-config #client",
+  HOST = "#csssql-config #host",
+  USER = "#csssql-config #user",
+  PASSWORD = "#csssql-config #password",
+  DB = "#csssql-config #db"
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
-export class AMMuenchTSApp {
-	public helloWorld() {
-		return '🦈   Hello World, I am a typescript app';
-	}
-}
-const myApp = new AMMuenchTSApp();
+import * as knex from "knex";
+import { CONFIG_IDS } from "./enums/config.enum";
 
-console.log(myApp.helloWorld());
+export class CSSql {
+  private _bodyRef: HTMLBodyElement;
+  private _config: knex.Config;
+
+  constructor(body: HTMLBodyElement) {
+    this._bodyRef = body;
+
+    this._config = {
+      client: this._getStyleValue(this._bodyRef, CONFIG_IDS.CLIENT),
+      connection: {
+        host: this._getStyleValue(this._bodyRef, CONFIG_IDS.HOST),
+        user: this._getStyleValue(this._bodyRef, CONFIG_IDS.USER),
+        password: this._getStyleValue(this._bodyRef, CONFIG_IDS.PASSWORD),
+        database: this._getStyleValue(this._bodyRef, CONFIG_IDS.DB)
+      }
+    };
+  }
+
+  get config(): any {
+    return this.config;
+  }
+
+  private _getStyleValue(elem: Element, queryTarget: string): string {
+    try {
+      const htmlElement: HTMLElement = elem.querySelector(queryTarget);
+      return htmlElement.style.content;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+}
