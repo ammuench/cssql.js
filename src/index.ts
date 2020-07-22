15f5336f84b487fd54ea9cfc27b0fe6370c22e88
Alex Muench <ammuench@gmail.com>
2020-07-21 20:35:07 -0500

    🐛 Set bodyRef to be parentRef
    * Sets bodyRef to be parentRef of type generic HTMLElement

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import * as knex from "knex";
 import { CONFIG_IDS } from "./enums/config.enum";
 
 export class CSSql {
-  private _bodyRef: HTMLBodyElement;
+  private _parentRef: HTMLElement;
   private _config: knex.Config;
 
-  constructor(body: HTMLBodyElement) {
-    this._bodyRef = body;
+  constructor(body: HTMLElement) {
+    this._parentRef = body;
 
     this._config = {
-      client: this._getStyleValue(this._bodyRef, CONFIG_IDS.CLIENT),
+      client: this._getStyleValue(this._parentRef, CONFIG_IDS.CLIENT),
       connection: {
-        host: this._getStyleValue(this._bodyRef, CONFIG_IDS.HOST),
-        user: this._getStyleValue(this._bodyRef, CONFIG_IDS.USER),
-        password: this._getStyleValue(this._bodyRef, CONFIG_IDS.PASSWORD),
-        database: this._getStyleValue(this._bodyRef, CONFIG_IDS.DB)
+        host: this._getStyleValue(this._parentRef, CONFIG_IDS.HOST),
+        user: this._getStyleValue(this._parentRef, CONFIG_IDS.USER),
+        password: this._getStyleValue(this._parentRef, CONFIG_IDS.PASSWORD),
+        database: this._getStyleValue(this._parentRef, CONFIG_IDS.DB)
       }
     };
   }
